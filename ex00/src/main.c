d68510560a6afaa172f59e833542faf805263dc6
David Garces <davigarc@e1z2r2p4.42.us.org>
2019-07-28 19:40:23 -0700

    norm passed in main.c, parse, print

--- ex00/src/main.c
+++ ex00/src/main.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sengle <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: davigarc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/27 22:26:53 by sengle            #+#    #+#             */
-/*   Updated: 2019/07/27 22:40:28 by sengle           ###   ########.fr       */
+/*   Created: 2019/07/27 21:33:22 by davigarc          #+#    #+#             */
+/*   Updated: 2019/07/28 19:36:04 by davigarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "colle-2.h"
 
-#define	MAX_BUF	32768
+#define MAX_BUF	32768
 
 int	main(int argc, char **argv)
 {
@@ -20,9 +20,7 @@ int	main(int argc, char **argv)
 	char	*buf[MAX_BUF];
 
 	buf = malloc(MAX_BUF);
-
 	buf_size = read(0, &buf, MAX_BUF);
-
 	if (buf_size > 0)
 	{
 		parse_rush(buf);
