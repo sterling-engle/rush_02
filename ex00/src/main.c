96a6e3a864850b0eba707620e416a60ad2bb4caf
Sterling Engle <sengle@Zeus.localdomain>
2019-07-28 22:48:56 -0700

    fixed read problem

--- ex00/src/main.c
+++ ex00/src/main.c
@@ -6,7 +6,7 @@
 /*   By: davigarc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/27 22:26:53 by sengle            #+#    #+#             */
-/*   Updated: 2019/07/28 21:29:02 by sjiseong         ###   ########.fr       */
+/*   Updated: 2019/07/28 22:47:37 by sengle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,25 @@
 #include <sys/uio.h>
 #include <unistd.h>
 
-#define MAX_BUF	32768
+#define MAX_BUF	32767
 
 #include <stdio.h>
 
 int	main(int argc, char **argv)
 {
 	int		buf_size;
+	int		bytes_read;
 	char	*buf;
 
+	bytes_read = 0;
 	if (argc < 1)
 		return (1);
 	if (argv[0][0] == 0)
 		return (1);
-	buf = (char*)malloc(sizeof(char) * (MAX_BUF + 10000));
-	buf_size = read(0, buf, MAX_BUF);
-	buf[buf_size] = 0;
+	buf = (char*)malloc(sizeof(char) * MAX_BUF);
+	while ((buf_size = read(0, &buf[bytes_read], MAX_BUF)) > 0)
+		bytes_read += buf_size;
+	buf[bytes_read] = '\0';
 	parse_rush(buf);
 	free(buf);
 	return (0);
